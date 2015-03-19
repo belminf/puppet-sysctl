bb25773c213fc86e5b15f2360e1ae5c2ad82e5ec
Belmin Fernandez <me@iambelmin.com>
2015-03-19 09:10:49 -0400

    added example to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,18 @@
 #
 # == Sample Usage:
 #
-# class { 'sysctl': }
+#sysctl::set {
+#    'kernel.shmall':   value => 2097152;
+#    'kernel.shmmax':   value => '2147483648';
+#    'kernel.shmmni':   value => '4096';
+#    'kernel.sem':      value => '256 32000 100 142';
+#    'fs.file-max':     value => '131072';
+#    'kernel.msgmni':   value => '2878';
+#    'kernel.msgmax':   value => '8192';
+#    'kernel.msgmnb':   value => '65535';
+#    'net.ipv4.ip_local_port_range': value => '10000 65000';
+#}
+
 
 class sysctl {
 
